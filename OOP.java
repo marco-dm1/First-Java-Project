abd570d7605e3caf2b79f44882a1a9aed6a78df3
marcodm-1 <marcodiazmoore@gmail.com>
2022-01-15 02:00:24 -0400

    OOP Constructor overloading

--- OOP.java
+++ OOP.java
@@ -6,6 +6,9 @@ public class OOP {
         Person bob = new Person("Bob", 24);
         System.out.println(bob.getAge());
 
+        // Constructor overloading in use, only the single argument is needed.
+        Person john = new Person("John");
+
         // Static method does not rely on a new instance of the Person class being created.
         System.out.println(Person.staticMethodTest());
     }
