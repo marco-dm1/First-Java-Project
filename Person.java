e87beee43d4d9436a5f0fe21f8bc0f622ae6d336
marcodm-1 <marcodiazmoore@gmail.com>
2022-01-15 01:54:47 -0400

    More comments on OOP

--- Person.java
+++ Person.java
@@ -1,8 +1,12 @@
 package com.company;
 
 public class Person {
-    /* Encapsulation refers to the private variables below that are only accessible
-    by non-static methods inside this class. */
+    /*
+        - The state of an object is the object's variables.
+        - The behavior of an object are the object's methods.
+        - Encapsulation refers to the private variables below that are only accessible
+        by non-static methods inside this class.
+    */
     private String name;
     private int age;
 
